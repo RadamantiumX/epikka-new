84d2968a0465f0345570485acdcd3b96efd3816c
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-26 19:51:55 -0300

    adding the context

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css';
 import router from './router';
+import { ContextProvider } from './context/ContextForm';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ContextProvider>
      <RouterProvider router={router}/>
+    </ContextProvider>
   </React.StrictMode>,
 )
